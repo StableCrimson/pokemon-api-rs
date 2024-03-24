402295ab6ab0db905a537f5451a7aec0932874fe
rhyse302 <rhysethompson@gmail.com>
2024-03-24 10:12:26 -0500

    Update trainer settings and formatting

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,10 @@ mod combat;
 mod pokemon;
 mod trainer;
 
-use pokemon::{BaseStats, Pokemon, Stats, MoveSet, GrowthRate, PokemonSpecies, Type};
+use pokemon::{BaseStats, GrowthRate, MoveSet, Pokemon, PokemonSpecies, Stats, Type};
 use trainer::Trainer;
 
 fn main() {
-
     let trainer: Trainer = Trainer::new("Rhyse");
     println!("Hello, world!, {:?}", trainer);
 
@@ -16,16 +15,29 @@ fn main() {
     let mut evs = Stats::new(100, 100, 100, 100, 100);
     println!("{:?}", evs);
 
-    let opponent_stats = BaseStats::new(100, 100, 100, 100, 100); 
+    let opponent_stats = BaseStats::new(100, 100, 100, 100, 100);
     evs.calc_new_evs(&opponent_stats);
     println!("{:?}", evs);
 
     let base_stats = BaseStats::new(10, 5, 6, 8, 7);
     let initial_moves = MoveSet::empty();
-    let charizard_base = PokemonSpecies::new(3, "Charizard".to_string(), base_stats, Type::Fire, Type::Flying, 200, 200, GrowthRate::Fast, initial_moves);
-    let charizard = Pokemon::new(charizard_base, 82, trainer, None, None).unwrap_or_else(|err| panic!("{}", err));
-    println!("{}", charizard.exp_for_next_level());
+    let charizard_base = PokemonSpecies::new(
+        3,
+        "Charizard".to_string(),
+        base_stats,
+        Type::Fire,
+        Type::Flying,
+        200,
+        200,
+        GrowthRate::Fast,
+        initial_moves,
+    );
 
+    // TODO: Unwrap or default?
+    let charizard = Pokemon::new(charizard_base, 82, &trainer, None, None)
+        .unwrap_or_else(|err| panic!("{}", err));
+    println!("{}", charizard.exp_for_next_level());
+    println!("{:?}", charizard.get_original_trainer_id());
 }
 
 // What should the API look like?
@@ -33,6 +45,6 @@ fn main() {
 // let charizard: Pokemon = Pokemon::new(Pokedex::Charizard, level: 4, moves: vec![Move::Scratch]);
 // charizard.get_stats();
 // charizard.level_up();
-// 
+//
 // let ash: Trainer = Trainer::new("ASH");
 // ash.add_to_party(&mut charizard);
